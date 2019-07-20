d21fe29605362522a759ce35365505b9934830d2
kamauvick <kamauvick@outlook.com>
2019-07-20 20:42:03 +0300

    Added variables for cacheing the form elements.

--- js/script.js
+++ js/script.js
@@ -1,35 +1,37 @@
 $(function () {
-    $('#desp').hide();
-    $('#devp').hide();
-    $('#prop').hide();
+    let desp = $('#desp');
+    let devp = $('#devp');
+    let prop = $('#prop');
+    $(desp).hide();
+    $(devp).hide();
+    $(prop).hide();
 
     //Show paragraph content
     $('.des').on('click', function () {
         $('.des').slideUp('slow');
-        $('#desp').show(500);
-
+        $(desp).show(500);
     });
     $('.dev').on('click', function () {
         $('.dev').slideUp('slow');
-        $('#devp').show(500);
+        $(devp).show(500);
     });
     $('.pro').on('click', function () {
         $('.pro').slideUp('slow');
-        $('#prop').show(500);
-    })
+        $(prop).show(500);
+    });
 
     //Hide paragraph content
-    $('#desp').on('click', function () {
+    $(desp).on('click', function () {
         $('.des').slideDown();
-        $('#desp').hide(500);
+        $(desp).hide(500);
     });
-    $('#devp').on('click', function () {
+    $(devp).on('click', function () {
         $('.dev').slideDown();
-        $('#devp').hide(500);
+        $(devp).hide(500);
     });
-    $('#prop').on('click', function () {
+    $(prop).on('click', function () {
         $('.pro').slideDown();
-        $('#prop').hide(500);
+        $(prop).hide(500);
     })
 
     //Overlay images
