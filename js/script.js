51301835769ca1ab370ef4d56166f9fd2b4b4df4
kamauvick <kamauvick@outlook.com>
2019-07-20 21:49:13 +0300

    Fix bolding and unbolding services headers.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ $(function () {
     let desp = $('#desp');
     let devp = $('#devp');
     let prop = $('#prop');
+    let destext = $('#destxt');
+    let devtext = $('#devtext');
+    let protext = $('#protext');
+
     $(desp).hide();
     $(devp).hide();
     $(prop).hide();
@@ -10,28 +14,35 @@ $(function () {
     $('.des').on('click', function () {
         $('.des').slideUp('slow');
         $(desp).show(500);
+        $(destext).animate({fontWeight:'bolder',fontSize:'30px'})
+
     });
     $('.dev').on('click', function () {
         $('.dev').slideUp('slow');
         $(devp).show(500);
+        $(devtext).animate({fontWeight:'bolder',fontSize:'30px'})
     });
     $('.pro').on('click', function () {
         $('.pro').slideUp('slow');
         $(prop).show(500);
+        $(protext).animate({fontWeight:'bolder',fontSize:'30px'})
     });
 
     //Hide paragraph content
     $(desp).on('click', function () {
         $('.des').slideDown();
         $(desp).hide(500);
+        $(destext).animate({fontWeight:'normal',fontSize:'16px'})
     });
     $(devp).on('click', function () {
         $('.dev').slideDown();
         $(devp).hide(500);
+        $(devtext).animate({fontWeight:'normal',fontSize:'16px'})
     });
     $(prop).on('click', function () {
         $('.pro').slideDown();
         $(prop).hide(500);
+        $(protext).animate({fontWeight:'normal',fontSize:'16px'})
     })
 
     //Overlay images
