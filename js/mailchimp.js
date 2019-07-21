b55d6a790effc7aa5e23e56375985a954c1e3fbe
kamauvick <kamauvick@outlook.com>
2019-07-21 20:21:44 +0300

    Add javascript file to handle mailchimp intergration.

--- js/mailchimp.js
+++ js/mailchimp.js
@@ -1,16 +1,1 @@
-$(function () {
-    let name = $("input#name").val();
-    let email = $("input#email").val();
-    let message = $("textarea#message").val();
-
-    $("form#form").submit(function(event){
-        event.preventDefault();
-        if (name.val() && email.val()){
-            alert ("Hello " + name + ", Thanks for reaching out...Well get back to you very soon..Cheers!");
-        }
-        else {
-            alert("Please provide your correct name and email!");
-        }
-    });
-
-});
+let APIkey = 'ef50c141cfec2b466e42cdaa95b572b9-us3';
